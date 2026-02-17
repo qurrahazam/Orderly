b9dfb502133421806703b405a4f9cc0216afaecc
qurrahazam <qurrahazam@users.noreply.github.com>
2026-02-17 07:47:24 +0000

    chore: auto prisma migration 20260217074724_auto_migration

---
+++ prisma/migrations/20260217074724_auto_migration/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "public"."Test" ADD COLUMN     "test4" TEXT,
+ADD COLUMN     "test5" TEXT,
+ADD COLUMN     "test6" TEXT,
+ADD COLUMN     "test7" TEXT,
+ADD COLUMN     "test8" TEXT,
+ADD COLUMN     "test9" TEXT;
+
